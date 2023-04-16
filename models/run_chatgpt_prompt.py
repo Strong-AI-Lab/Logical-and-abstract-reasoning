33bedea5e6b24514746155e99a36b5c4c4942035
Qiming Bao <qbao775@aucklanduni.ac.nz>
2023-04-16 11:31:16 +0000

    update chatgpt

--- models/run_chatgpt_prompt.py
+++ models/run_chatgpt_prompt.py
@@ -3,7 +3,7 @@ import openai
 import json
 import pandas as pd
 from tqdm import tqdm
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "OPENAI_API_KEY"
 
 flag = "reclor"
 response_list = {'context':[],'question':[],'optionA':[], 'optionB':[],'optionC':[],'optionD':[],'predict_answer':[]}
@@ -17,13 +17,16 @@ if flag == "reclor":
         optionB = item["answers"][1]
         optionC = item["answers"][2]
         optionD = item["answers"][3]
-        input_prompt = "Instruction: Please only generate A, B, C or D as your predicted answer for the following input." + \
-            " Given context: " + context + " Question: " + \
+        input_prompt = " Given context: " + context + " Question: " + \
             " A: " + optionA + " B: " + optionB + " C: " + optionC + \
             " D: " + optionD
-        response = openai.Completion.create(
+        response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=input_prompt,
+            # prompt=input_prompt,
+            messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Please only generate A, B, C or D as your predicted answer for the following input."},
+            {"role": "user", "content" : "How are you?"},
+            {"role": "assistant", "content" : "I am doing well"},
+            {"role": "user", "content" : input_prompt}],
             temperature=0.7,
             max_tokens=256,
             top_p=1,
@@ -36,7 +39,7 @@ if flag == "reclor":
         response_list["optionB"].append(optionB)
         response_list["optionC"].append(optionC)
         response_list["optionD"].append(optionD)
-        response_list["predict_answer"].append(response)
+        response_list["predict_answer"].append(response["choices"][0]["message"]["content"])
     df = pd.DataFrame(response_list)
     df.to_excel("chatgpt_reclor_prediction.xlsx")
         
@@ -52,13 +55,16 @@ elif flag == "logiqa":
         optionB = item["answers"][1]
         optionC = item["answers"][2]
         optionD = item["answers"][3]
-        input_prompt = "Instruction: Please only generate A, B, C or D as your predicted answer for the following input." + \
-            " Given context: " + context + " Question: " + \
+        input_prompt = " Given context: " + context + " Question: " + \
             " A: " + optionA + " B: " + optionB + " C: " + optionC + \
             " D: " + optionD
-        response = openai.Completion.create(
+        response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=input_prompt,
+            # prompt=input_prompt,
+            messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Please only generate A, B, C or D as your predicted answer for the following input."},
+            {"role": "user", "content" : "How are you?"},
+            {"role": "assistant", "content" : "I am doing well"},
+            {"role": "user", "content" : input_prompt}],
             temperature=0.7,
             max_tokens=256,
             top_p=1,
@@ -71,7 +77,7 @@ elif flag == "logiqa":
         response_list["optionB"].append(optionB)
         response_list["optionC"].append(optionC)
         response_list["optionD"].append(optionD)
-        response_list["predict_answer"].append(response)
+        response_list["predict_answer"].append(response["choices"][0]["message"]["content"])
     df = pd.DataFrame(response_list)
     df.to_excel("chatgpt_logiqa_prediction.xlsx")
         
@@ -89,13 +95,16 @@ elif flag == "logiqav2":
         optionB = item["options"][1]
         optionC = item["options"][2]
         optionD = item["options"][3]
-        input_prompt = "Instruction: Please only generate A, B, C or D as your predicted answer for the following input." + \
-            " Given context: " + context + " Question: " + \
+        input_prompt = " Given context: " + context + " Question: " + \
             " A: " + optionA + " B: " + optionB + " C: " + optionC + \
             " D: " + optionD
-        response = openai.Completion.create(
+        response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=input_prompt,
+            # prompt=input_prompt,
+            messages = [{"role": "system", "content" : "You are ChatGPT, a large language model trained by OpenAI. Answer as concisely as possible. Please only generate A, B, C or D as your predicted answer for the following input."},
+            {"role": "user", "content" : "How are you?"},
+            {"role": "assistant", "content" : "I am doing well"},
+            {"role": "user", "content" : input_prompt}],
             temperature=0.7,
             max_tokens=256,
             top_p=1,
@@ -108,6 +117,6 @@ elif flag == "logiqav2":
         response_list["optionB"].append(optionB)
         response_list["optionC"].append(optionC)
         response_list["optionD"].append(optionD)
-        response_list["predict_answer"].append(response)
+        response_list["predict_answer"].append(response["choices"][0]["message"]["content"])
     df = pd.DataFrame(response_list)
     df.to_excel("chatgpt_logiqav2_prediction.xlsx")
