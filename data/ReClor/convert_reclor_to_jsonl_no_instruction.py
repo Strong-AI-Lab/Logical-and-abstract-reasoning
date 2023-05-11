f6d1ab62b8c65047e132d56dbed0162428fa4699
Qiming Bao <qbao775@aucklanduni.ac.nz>
2023-05-11 00:44:51 +0000

    update reclor instruction

--- data/ReClor/convert_reclor_to_jsonl_no_instruction.py
+++ data/ReClor/convert_reclor_to_jsonl_no_instruction.py
@@ -1,11 +1,11 @@
 import json
 
 # read JSON file
-with open("/data/qbao775/Logical-and-abstract-reasoning/data/ReClor/test.json", "r", encoding="utf-8") as input_file:
+with open("/data/qbao775/Logical-and-abstract-reasoning/data/ReClor/val.json", "r", encoding="utf-8") as input_file:
     data = json.load(input_file)
-flag = "test"
+flag = "val"
 # save to a new JSONL file
-with open("/data/qbao775/Logical-and-abstract-reasoning/data/ReClor/ReClor_test_no_instruction.jsonl", "w", encoding="utf-8") as output_file:
+with open("/data/qbao775/Logical-and-abstract-reasoning/data/ReClor/ReClor_val_no_instruction.jsonl", "w", encoding="utf-8") as output_file:
     for item in data:
         temp = {"input": [{"role": "system", "content": None},
                         {"role": "user", "content": None},
@@ -14,15 +14,17 @@ with open("/data/qbao775/Logical-and-abstract-reasoning/data/ReClor/ReClor_test_
                         {"role": "user", "content": None},
                         {"role": "user", "content": None}], 
                 "choice_strings": ["A", "B", "C", "D"],
-                "ideal": None}
-        temp["input"][0]["content"] = "Passage: " + item["context"]
-        temp["input"][1]["content"] = "Question: " + item["question"]
+                "ideal": None,
+                "id_string": None}
+        temp["input"][0]["content"] = item["context"]
+        temp["input"][1]["content"] = item["question"] + " Please only return the letter in front of your predict correct option, A, B, C or D."
         temp["input"][2]["content"] = "A. " + item["answers"][0]
         temp["input"][3]["content"] = "B. " + item["answers"][1]
         temp["input"][4]["content"] = "C. " + item["answers"][2]
         temp["input"][5]["content"] = "D. " + item["answers"][3]
-        label = None
-        if flag != "test":
+        if flag == "test":  
+            label = None
+        else:
             if item["label"] == 0:
                 label = "A"
             elif item["label"] == 1:
@@ -32,5 +34,6 @@ with open("/data/qbao775/Logical-and-abstract-reasoning/data/ReClor/ReClor_test_
             elif item["label"] == 3:
                 label = "D"
         temp["ideal"] = label
+        temp["id_string"] = item["id_string"]
         json.dump(temp, output_file, ensure_ascii=False)
         output_file.write("\n")
